f352b71d490510728599d3754490285fbba8720b
bugsounet <github@bugsounet.fr>
2024-02-16 00:41:35 +0100

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,33 @@
 module.exports = {
-  "extends": ["eslint:recommended", "plugin:@stylistic/all-extends", "plugin:import/recommended"],
-  "env": {
-    "es2023": true,
-    "node": true
+  extends: ["eslint:recommended", "plugin:@stylistic/all-extends", "plugin:import/recommended"],
+  env: {
+    es2023: true,
+    node: true
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2023,
-      "ecmaFeatures": {
-        "globalReturn": true
-      }
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2023,
+    ecmaFeatures: {
+      globalReturn: true
+    }
   },
-  "globals": {
-    "config": true,
-    "Log": true,
-    "MM": true,
-    "Module": true,
-    "moment": true,
-    "document": true
+  globals: {
+    config: true,
+    Log: true,
+    MM: true,
+    Module: true,
+    moment: true,
+    document: true
   },
-  "rules": {
-    "eqeqeq": "error",
+  rules: {
+    eqeqeq: "error",
     "import/order": "error",
-    "import/extensions": ["error",
+    "import/extensions": [
+      "error",
       "ignorePackages",
-        {
-          "json": "always" // ignore json require (display EXT version and rev date)
-        }
+      {
+        json: "always" // ignore json require (display EXT version and rev date)
+      }
     ],
     "import/newline-after-import": "error",
     "no-param-reassign": "error",
@@ -44,20 +45,20 @@ module.exports = {
     "@stylistic/function-call-argument-newline": ["error", "consistent"],
     "@stylistic/function-paren-newline": ["error", "consistent"],
     "@stylistic/implicit-arrow-linebreak": ["error", "beside"],
-    "@stylistic/max-statements-per-line": ["error", { "max": 2 }],
+    "@stylistic/max-statements-per-line": ["error", { max: 2 }],
     "@stylistic/multiline-ternary": ["error", "always-multiline"],
-    "@stylistic/newline-per-chained-call": ["error", { "ignoreChainWithDepth": 4 }],
+    "@stylistic/newline-per-chained-call": ["error", { ignoreChainWithDepth: 4 }],
     "@stylistic/no-extra-parens": "off",
     "@stylistic/no-tabs": "off",
     "@stylistic/object-curly-spacing": ["error", "always"],
-    "@stylistic/object-property-newline": ["error", { "allowAllPropertiesOnSameLine": true }],
+    "@stylistic/object-property-newline": ["error", { allowAllPropertiesOnSameLine: true }],
     "@stylistic/operator-linebreak": ["error", "before"],
     "@stylistic/padded-blocks": "off",
     "@stylistic/quote-props": ["error", "as-needed"],
     "@stylistic/quotes": ["error", "double"],
-    '@stylistic/indent': ['error', 2], // indent 2 spaces
+    "@stylistic/indent": ["error", 2], // indent 2 spaces
     "@stylistic/semi": ["error", "always"],
     "@stylistic/space-before-function-paren": ["error", "always"],
     "@stylistic/spaced-comment": "off"
   }
-}
+};
