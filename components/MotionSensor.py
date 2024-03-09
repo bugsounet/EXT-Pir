3612fe3b4a7049992def675d16369839654d0831
Bugsounet - Cédric <github@bugsounet.fr>
2024-03-09 22:15:07 +0100

    Fix limited Gpio number

--- components/MotionSensor.py
+++ components/MotionSensor.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(
 def gpio_check(x):
     x = int(x)
     if x < 1 or x > 21:
-        raise argparse.ArgumentTypeError("GPIO must be between 1 and 21")
+        raise argparse.ArgumentTypeError("GPIO must be between 1 and 29")
     return x
 
 parser.add_argument("-g", "--gpio", help="Define GPIO", type=gpio_check, required=True)
