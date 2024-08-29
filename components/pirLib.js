e066a56189354e9d99920d071a3af398f4c1894c
bugsounet <github@bugsounet.fr>
2024-08-30 01:02:47 +0200

    cleaning

--- components/pirLib.js
+++ components/pirLib.js
@@ -179,22 +179,10 @@ class PIR {
 
   /* experimental */
 
-  async gpiodDetect () {
+  gpiodDetect () {
     try {
       const { version, Chip, Line } = require("node-libgpiod");
       const numbers = [0,1,2,3,4,5,6,7,8,9,10];
-
-      /*
-      numbers.forEach((number) => {
-        try {
-          const chip = new Chip(number);
-          if (chip.getChipLabel().includes("pinctrl-")) {
-            console.log(`[PIR] [CORE] [GPIOD] Found chip ${number}: ${chip.getChipLabel()}`);
-            this.pirChipNumber = number;
-          }
-        } catch { }
-      });
-*/
 
       numbers.every((number) => {
         try {
