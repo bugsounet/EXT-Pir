d840a22d8758eb1c21d11660f96d079939b6e2e7
bugsounet <github@bugsounet.fr>
2024-09-06 15:17:22 +0200

    delete mode 0 and 1

--- components/pirLib.js
+++ components/pirLib.js
@@ -1,6 +1,6 @@
 /*******************
 * PIR library
-* bugsounet ©08/24
+* bugsounet ©09/24
 *******************/
 
 var log = (...args) => { /* do nothing */ };
@@ -29,26 +29,18 @@ class PIR {
     if (this.running) return;
     switch (this.config.mode) {
       case 0:
-        console.log("[PIR] [CORE] Mode 0 Selected (onoff library)");
-        this.onoffDetect();
-        break;
-      case 1:
-        console.log("[PIR] [CORE] Mode 1 Selected (rpi.gpio)");
-        this.gpioDetect();
-        break;
-      case 2:
-        console.log("[PIR] [CORE] Mode 2 Selected (gpiozero)");
-        this.gpiozeroDetect();
-        break;
-      case 3:
         console.log("[PIR] [CORE] Mode 3 Selected (gpiod library)");
         this.gpiodDetect();
+        break;
+      case 1:
+        console.log("[PIR] [CORE] Mode 2 Selected (gpiozero)");
+        this.gpiozeroDetect();
         break;
       default:
         console.warn(`[PIR] [CORE] mode: ${this.config.mode} is not a valid value`);
         console.warn("[PIR] [CORE] set mode 0");
         this.config.mode = 0;
-        this.onoffDetect();
+        this.gpiodDetect();
         break;
     }
   }
@@ -67,73 +59,6 @@ class PIR {
     this.running = false;
     this.callback("PIR_STOP");
     log("Stop");
-  }
-
-  onoffDetect () {
-    try {
-      const Gpio = require("onoff").Gpio;
-      this.pir = new Gpio(this.config.gpio, "in", "both");
-      this.callback("PIR_STARTED");
-      console.log("[PIR] [CORE] Started!");
-    } catch (err) {
-      console.error(`[PIR] [CORE] ${err}`);
-      this.running = false;
-      return this.callback("PIR_ERROR", err.message);
-    }
-    this.running = true;
-    this.pir.watch((err, value) => {
-      if (err) {
-        console.error(`[PIR] [CORE] ${err}`);
-        return this.callback("PIR_ERROR", err.message);
-      }
-      log(`Sensor read value: ${value}`);
-      if (value === 1) {
-        this.callback("PIR_DETECTED");
-        log(`Detected presence (value: ${value})`);
-      }
-    });
-  }
-
-  gpioDetect () {
-    const { PythonShell } = require("python-shell");
-    let options = {
-      mode: "text",
-      scriptPath: __dirname,
-      pythonOptions: ["-u"],
-      args: [ "-g", this.config.gpio ]
-    };
-
-    this.pir = new PythonShell("gpioSensor.py", options);
-    this.callback("PIR_STARTED");
-    console.log("[PIR] [CORE] Started!");
-    this.running = true;
-
-    this.pir.on("message", (message) => {
-      // detect pir
-      if (message === "Detected") {
-        log("Detected presence");
-        this.callback("PIR_DETECTED");
-      } else {
-        console.error("[PIR] [CORE]", message);
-        this.callback("PIR_ERROR", message);
-        this.running = false;
-      }
-    });
-
-    this.pir.on("stderr", (stderr) => {
-      // handle stderr (a line of text from stderr)
-      if (this.config.debug) console.error("[PIR] [CORE]", stderr);
-      this.running = false;
-    });
-
-    this.pir.end((err,code,signal) => {
-      if (err) {
-        console.error("[PIR] [CORE] [PYTHON]",err);
-        this.callback("PIR_ERROR", err.message);
-      }
-      console.warn(`[PIR] [CORE] [PYTHON] The exit code was: ${code}`);
-      console.warn(`[PIR] [CORE] [PYTHON] The exit signal was: ${signal}`);
-    });
   }
 
   gpiozeroDetect () {
@@ -176,8 +101,6 @@ class PIR {
       console.warn(`[PIR] [CORE] [PYTHON] The exit signal was: ${signal}`);
     });
   }
-
-  /* experimental */
 
   gpiodDetect () {
     try {
